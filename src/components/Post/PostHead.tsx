2570118a858aefaaa4b9a757e282dde810853f66
2Zerozero <hawgae@gmail.com>
2025-01-01 20:11:38 +0900

    feat: 게시글 상단 레이아웃 구현

---
+++ src/components/Post/PostHead.tsx
@@ -0,0 +1,42 @@
+import styled from '@emotion/styled'
+import { GatsbyImage, IGatsbyImageData } from 'gatsby-plugin-image'
+import React, { FunctionComponent } from 'react'
+
+// Type
+type GatsbyImgProps = {
+  image: IGatsbyImageData
+  alt: string
+  classname?: string
+}
+
+type PostHeadProps = {
+  thumbnail: IGatsbyImageData
+}
+
+// CSS
+const PostHeadWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 400px;
+`
+
+const BackgroundImage = styled((props: GatsbyImgProps) => (
+  <GatsbyImage {...props} style={{ position: 'absolute' }} />
+))`
+  z-index: -1;
+  width: 100%;
+  height: 400px;
+  object-fit: cover;
+  filter: brightness(0.25);
+`
+
+// Component
+const PostHead: FunctionComponent<PostHeadProps> = function ({ thumbnail }) {
+  return (
+    <PostHeadWrapper>
+      <BackgroundImage image={thumbnail} alt="thumbnail" />
+    </PostHeadWrapper>
+  )
+}
+
+export default PostHead
