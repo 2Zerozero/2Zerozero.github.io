49b90977b87e2f377407b2d5d293ccdfa5e49661
2Zerozero <hawgae@gmail.com>
2025-01-05 22:39:42 +0900

    feat: Utterances 위젯 추가 및 적용 완료

---
+++ src/components/Post/CommentWidget.tsx
@@ -0,0 +1,52 @@
+import React, { FunctionComponent, useEffect, useRef } from 'react'
+
+// Setting
+const src = 'https://utteranc.es/client.js' // Utterances 스크립트 소스 URL
+const repo = '2Zerozero/2Zerozero.github.io' // 댓글이 저장될 GitHub 레포지토리
+
+// Type
+type UtterancesAttributesType = {
+  src: string
+  repo: string
+  'issue-term': string
+  label: string
+  theme: string
+  crossorigin: string
+  async: string
+}
+
+// Component
+const CommentWidget: FunctionComponent = function () {
+  // 작동 방식
+  // 1. div 요소에 대한 ref를 생성
+  // 2. useEffect 내에서 script 엘리먼트를 생성
+  // 3. 필요한 속성들을 script 엘리먼트에 설정
+  // 4. ref로 참조된 div에 script 엘리먼트를 추가하여 댓글 위젯 렌더링
+  const element = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (element.current === null) return
+
+    const utterances: HTMLScriptElement = document.createElement('script')
+
+    const attributes: UtterancesAttributesType = {
+      src, // Utterances 스크립트 소스 URL
+      repo, // 댓글이 저장될 GitHub 레포지토리
+      'issue-term': 'pathname', // 댓글을 구분하는 기준(pathname 사용)
+      label: 'Comment', // 이슈에 붙을 라벨
+      theme: `github-light`, // 댓글 위젯의 테마
+      crossorigin: 'anonymous', // CORS 설정
+      async: 'true', // 비동기 로딩 설정
+    }
+
+    Object.entries(attributes).forEach(([key, value]) => {
+      utterances.setAttribute(key, value)
+    })
+
+    element.current.appendChild(utterances)
+  }, [])
+
+  return <div ref={element} />
+}
+
+export default CommentWidget
