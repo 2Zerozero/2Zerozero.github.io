b0b63c8ffd47695518c54322a943c74c86a54332
2Zerozero <hawgae@gmail.com>
2025-01-15 13:57:01 +0900

    [feat] Title 변경

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `2Zerozero Frontend Developer Blog`,
+    title: `주니어 개발자의 성장기록`,
     description: `주니어 개발자로서 성장하는 과정을 기록하는 블로그입니다.`,
     author: `2Zerozero`,
     siteUrl: `https://2zerozero.github.io`,
