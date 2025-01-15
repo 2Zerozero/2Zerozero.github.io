ae965264c756a80f0e3eaf2695413ca2d5f761cc
2Zerozero <hawgae@gmail.com>
2025-01-15 13:43:03 +0900

    Updates

---
+++ component---src-pages-info-tsx-e7fddfef8a46f5c43c5a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[171],{2751:function(t,e,a){a.r(e);var s=a(7437);const n={name:"y24uj",styles:"*{margin:0;padding:0;box-sizing:border-box;font-size:20px;}"};e.default=function(t){let{data:{site:{siteMetadata:{title:e,description:a,author:i}}}}=t;return(0,s.Y)("div",null,(0,s.Y)(s.mL,{styles:n}),e,a," ",i)}}}]);
+//# sourceMappingURL=component---src-pages-info-tsx-e7fddfef8a46f5c43c5a.js.map
